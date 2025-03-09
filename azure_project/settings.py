110f0c557e911fd822f92c4b2799827aec0ee159
kracst <stefbraescu232@gmail.com>
2025-03-09 23:14:15 +0200

    added crispy bootstrap to settings.py

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'crispy_bootstrap4'
     'azure_content.apps.AzureContentConfig', 
     
 ]
